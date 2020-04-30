860cd02be8322cfe7a83c0f76bde8970139136c4
Allan Mercou <allan.mercou@icloud.com>
2020-04-30 05:24:12 +0200

    Adds ddragon GetVersions method.

---
+++ src/RiotPls.DataDragon/DataDragonClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RiotPls.DataDragon
+{
+    public class DataDragonClient
+    {
+        public const string Host = "https://ddragon.leagueoflegends.com";
+        public const string Api = "/api";
+        public const string Cdn = "/cdn";
+
+        private readonly HttpClient _client;
+        
+        public DataDragonClient(HttpClient client = null)
+        {
+            _client = client ?? new HttpClient();
+        }
+        
+        public async Task<IReadOnlyCollection<string>> GetVersionsAsync()
+        {
+            var request = await _client.GetStreamAsync($"{Host}{Api}/versions.json");
+            return await JsonSerializer.DeserializeAsync<IReadOnlyCollection<string>>(request);
+        }
+    }
+}
