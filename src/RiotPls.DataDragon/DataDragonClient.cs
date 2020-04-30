3a3cbcc618057f1dee38102f5380822b6e983cbb
Allan Mercou <allan.mercou@icloud.com>
2020-04-30 10:30:47 +0200

    Adds base champion static data.

--- src/RiotPls.DataDragon/DataDragonClient.cs
+++ src/RiotPls.DataDragon/DataDragonClient.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using RiotPls.DataDragon.Entities;
 
 namespace RiotPls.DataDragon
 {
@@ -17,11 +18,34 @@ namespace RiotPls.DataDragon
         {
             _client = client ?? new HttpClient();
         }
-        
+
+        /// <summary>
+        /// Returns a list of every available version of Data Dragon.
+        /// </summary>
         public async Task<IReadOnlyCollection<string>> GetVersionsAsync()
         {
             var request = await _client.GetStreamAsync($"{Host}{Api}/versions.json");
             return await JsonSerializer.DeserializeAsync<IReadOnlyCollection<string>>(request);
+        }
+        
+        /// <summary>
+        /// Returns a list of every available languages for this version of Data Dragon.
+        /// </summary>
+        public async Task<IReadOnlyCollection<string>> GetLanguagesAsync()
+        {
+            var request = await _client.GetStreamAsync($"{Host}{Cdn}/languages.json");
+            return await JsonSerializer.DeserializeAsync<IReadOnlyCollection<string>>(request);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="ChampionData"/> containing base information about every champion on the game.
+        /// </summary>
+        /// <param name="version">Version of Data Dragon.</param>
+        /// <param name="language">Language in which the data must be returned.</param>
+        public async Task<ChampionData> GetChampionsAsync(string version, string language = "en_US")
+        {
+            var request = await _client.GetStreamAsync($"{Host}{Cdn}/{version}/data/{language}/champion.json");
+            return await JsonSerializer.DeserializeAsync<ChampionData>(request);
         }
     }
 }
---
+++ src/RiotPls.DataDragon/Extensions/ChampionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using RiotPls.DataDragon.Entities;
+
+namespace RiotPls.DataDragon.Extensions
+{
+    public static class ChampionExtensions
+    {
+        /// <summary>
+        /// Gets a champion information by its id.
+        /// </summary>
+        /// <param name="client">Data Dragon client that performs the request.</param>
+        /// <param name="id">Id of the champion.</param>
+        /// <param name="version">Version of Data Dragon.</param>
+        /// <param name="language">Language in which the data must be returned.</param>
+        public static async Task<ChampionBase> GetChampionByIdAsync(this DataDragonClient client, 
+            string id, string version, string language = "en_US")
+        {
+            var champions = await client.GetChampionsAsync(version, language);
+            return champions.Champions[id];
+        }
+        
+        /// <summary>
+        /// Gets a champion information by its name.
+        /// </summary>
+        /// <param name="client">Data Dragon client that performs the request.</param>
+        /// <param name="name">Name of the champion.</param>
+        /// <param name="version">Version of Data Dragon.</param>
+        /// <param name="language">Language in which the data must be returned.</param>
+        public static async Task<ChampionBase> GetChampionByNameAsync(this DataDragonClient client, 
+            string name, string version, string language = "en_US")
+        {
+            var champions = await client.GetChampionsAsync(version, language);
+            return champions.Champions.Values.First(
+                x => x.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
