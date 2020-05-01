e4c231fecfb5256c0b22561bfbecd373f4731d6b
JustNrik <justnrik@gmail.com>
2020-05-01 16:27:29 -0400

    Implementing IEquatable and IComparable on GameVersion

--- src/RiotPls.DataDragon/Entities/GameVersion.cs
+++ src/RiotPls.DataDragon/Entities/GameVersion.cs
@@ -6,7 +6,7 @@ namespace RiotPls.DataDragon.Entities
     /// <summary>
     ///     Represents a game version for League of Legends, like "10.9.1".
     /// </summary>
-    public class GameVersion
+    public sealed class GameVersion : IEquatable<GameVersion>, IComparable<GameVersion>
     {
         /// <summary>
         ///     The major release version.
@@ -23,7 +23,7 @@ namespace RiotPls.DataDragon.Entities
         /// </summary>
         public int Patch { get; }
 
-        private readonly string _string;
+        private readonly string _version;
 
         private GameVersion(int major, int minor, int patch = -1)
         {
@@ -31,11 +31,11 @@ namespace RiotPls.DataDragon.Entities
             Minor = minor;
             Patch = patch;
 
-            _string = patch == -1
+            _version = patch == -1
                 ? $"lolpatch_{Major}.{Minor}"
                 : $"{Major}.{Minor}.{Patch}";
         }
-        
+
         /// <summary>
         ///     Parses a string input into a <see cref="GameVersion"/>.
         /// </summary>
@@ -47,7 +47,7 @@ namespace RiotPls.DataDragon.Entities
         public static GameVersion Parse(string input)
             => TryParse(input, out var gameVersion)
             ? gameVersion
-            : throw new InvalidOperationException($"Failed to parse game version \"{input}\".");
+            :  throw new InvalidOperationException($"Failed to parse game version \"{input}\".");
 
         /// <summary>
         ///     Parses a string input into a <see cref="GameVersion"/>.
@@ -55,9 +55,12 @@ namespace RiotPls.DataDragon.Entities
         /// <param name="input">The game version, in string form.</param>
         /// <param name="gameVersion">A parsed game version object.</param>
         /// <returns>True if parsing was successful, or false if an error occurred.</returns>
-        public static bool TryParse(string input, [MaybeNullWhen(false)] out GameVersion gameVersion)
+        public static bool TryParse(string input, [NotNullWhen(true)] out GameVersion? gameVersion)
         {
             gameVersion = null;
+
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
             
             ReadOnlySpan<char> inputSpan = input.StartsWith("lolpatch_", StringComparison.Ordinal) ? input.AsSpan(9) : input;
 
@@ -100,6 +103,47 @@ namespace RiotPls.DataDragon.Entities
         }
 
         public override string ToString() 
-            => _string;
+            => _version;
+
+        public override bool Equals(object? obj)
+            => Equals(obj as GameVersion);
+
+        public override int GetHashCode()
+            => HashCode.Combine(Major, Minor, Patch);
+
+        /// <inheritdoc/>
+        public bool Equals(GameVersion? other)
+            => other is GameVersion version
+            && Major == version.Major
+            && Minor == version.Minor
+            && Patch == version.Patch;
+
+        /// <inheritdoc/>
+        public int CompareTo(GameVersion? other)
+        {
+            if (other is null)
+                return -1;
+
+            if (Major > other.Major)
+                return 1;
+            else if (Major < other.Major)
+                return -1;
+            else
+            {
+                if (Minor > other.Minor)
+                    return 1;
+                else if (Minor < other.Minor)
+                    return -1;
+                else
+                {
+                    if (Patch > other.Patch)
+                        return 1;
+                    else if (Patch < other.Patch)
+                        return -1;
+
+                    return 0;
+                }
+            }
+        }
     }
 }
