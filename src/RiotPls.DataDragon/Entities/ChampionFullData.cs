bd278cbd5a97f866a0da48743ba095fea4cd3441
Allan Mercou <allan.mercou@icloud.com>
2020-05-04 08:58:48 +0200

    Adds dto and entities for championFull

---
+++ src/RiotPls.DataDragon/Entities/ChampionFullData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace RiotPls.DataDragon.Entities
+{
+    public class ChampionFullData : BaseData
+    {
+        /// <summary>
+        ///     A dictionary of champion objects, keyed by their unique identifiers.
+        /// </summary>
+        public ReadOnlyDictionary<int, Champion> Champions { get; }
+
+        internal ChampionFullData(ChampionFullDataDto dto) : base(dto)
+        {
+            Champions = new ReadOnlyDictionary<int, Champion>(
+                dto.Champions.ToDictionary(
+                    x => int.Parse(x.Value.Key), y => new Champion(y.Value)));
+        }
+    }
+}
---
+++ src/RiotPls.DataDragon/Entities/DTOs/ChampionFullDataDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RiotPls.DataDragon.Entities
+{
+    internal class ChampionFullDataDto : BaseDataDto
+    {
+        [JsonPropertyName("data")]
+        public IReadOnlyDictionary<string, ChampionDto> Champions { get; set; }
+        
+        [JsonPropertyName("keys")]
+        public IReadOnlyDictionary<int, string> ChampionKeys { get; set; }
+    }
+}
