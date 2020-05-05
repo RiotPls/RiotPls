c85ca2650e65efa119d7f80cf7cc37ce35a26afc
JustNrik <justnrik@gmail.com>
2020-05-04 22:24:17 -0400

    added abstraction for DataDragonClient, removed closure, some more clean up

--- src/RiotPls.DataDragon/DataDragonClient.cs
+++ src/RiotPls.DataDragon/DataDragonClient.cs
@@ -10,7 +10,7 @@ using RiotPls.DataDragon.Helpers;
 
 namespace RiotPls.DataDragon
 {
-    public sealed class DataDragonClient : IDisposable
+    public sealed class DataDragonClient : IDataDragonClient
     {
         public const string Host = "https://ddragon.leagueoflegends.com";
         public const string Api = "/api";
@@ -46,7 +46,7 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a list of every available version of Data Dragon.
+        ///    Returns a list of every available version of Data Dragon.
         /// </summary>
         public ValueTask<IReadOnlyList<GameVersion>> GetVersionsAsync()
         {
@@ -56,21 +56,21 @@ namespace RiotPls.DataDragon
                 return ValueTaskHelper.Create(
                       !_options.Versions.IsExpired,
                       this,
-                      @this => @this._options.Versions.Data!,
-                      async @this =>
+                      client => client._options.Versions.Data!,
+                      async client =>
                       {
-                          var data = await @this.MakeRequestAsync<IReadOnlyList<GameVersion>>(
+                          var data = await client.MakeRequestAsync<IReadOnlyList<GameVersion>>(
                               $"{Api}/versions.json").ConfigureAwait(false);
 
-                          @this._options.Versions.Data = data;
+                          client._options.Versions.Data = data;
                           return data;
                       });
             }
         }
 
         /// <summary>
-        ///     Returns a list of every available language for the latest version
-        ///     of Data Dragon, expressed as UTF-8 culture codes. (i.e. en_US)
+        ///    Returns a list of every available language for the latest version
+        ///    of Data Dragon, expressed as UTF-8 culture codes. (i.e. en_US)
         /// </summary>
         public ValueTask<IReadOnlyList<GameLanguage>> GetLanguagesAsync()
         {
@@ -80,24 +80,24 @@ namespace RiotPls.DataDragon
                 return ValueTaskHelper.Create(
                       !_options.Languages.IsExpired,
                       this,
-                      @this => @this._options.Languages.Data!,
-                      async @this =>
+                      client => client._options.Languages.Data!,
+                      async client =>
                       {
-                          var data = await @this.MakeRequestAsync<IReadOnlyList<GameLanguage>>(
+                          var data = await client.MakeRequestAsync<IReadOnlyList<GameLanguage>>(
                               $"{Cdn}/languages.json").ConfigureAwait(false);
 
-                          @this._options.Languages.Data = data;
+                          client._options.Languages.Data = data;
                           return data;
                       });
             }
         }
 
         /// <summary>
-        ///     Returns a <see cref="ChampionBaseData"/> containing base information
-        ///     about every champion on the game.
+        ///    Returns a <see cref="ChampionBaseData"/> containing base information
+        ///    about every champion on the game.
         /// </summary>
         /// <param name="version">
-        ///     The version of Data Dragon to use.
+        ///    The version of Data Dragon to use.
         /// </param>
         public ValueTask<ChampionBaseData> GetPartialChampionsAsync(GameVersion version)
         {
@@ -109,14 +109,14 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a <see cref="ChampionBaseData"/> containing base information
-        ///     about every champion on the game.
+        ///    Returns a <see cref="ChampionBaseData"/> containing base information
+        ///    about every champion on the game.
         /// </summary>
         /// <param name="version">
-        ///     The version of Data Dragon to use.
+        ///    The version of Data Dragon to use.
         /// </param>
         /// <param name="language">
-        ///     The language in which the data must be returned. Defaults to English (United States).
+        ///    The language in which the data must be returned. Defaults to English (United States).
         /// </param>
         public ValueTask<ChampionBaseData> GetPartialChampionsAsync(GameVersion version, GameLanguage language)
         {
@@ -130,9 +130,10 @@ namespace RiotPls.DataDragon
                       async state =>
                       {
                           var (client, version, language) = state;
-                          var data = await client.MakeRequestAsync<ChampionBaseDataDto, ChampionBaseData>(
+                          var data = await client.MakeRequestAsync<DataDragonClient, ChampionBaseDataDto, ChampionBaseData>(
                               $"{Cdn}/{version}/data/{language}/champion.json", 
-                              dto => new ChampionBaseData(client, dto)).ConfigureAwait(false);
+                              client,
+                              (client, dto) => new ChampionBaseData(client, dto)).ConfigureAwait(false);
 
                           client._options.PartialChampions.Data = data;
                           return data;
@@ -141,11 +142,11 @@ namespace RiotPls.DataDragon
         }
         
         /// <summary>
-        ///     Returns a <see cref="ChampionFullData"/> containing full information
-        ///     about every champion on the game.
+        ///    Returns a <see cref="ChampionFullData"/> containing full information
+        ///    about every champion on the game.
         /// </summary>
         /// <param name="version">
-        ///     The version of Data Dragon to use.
+        ///    The version of Data Dragon to use.
         /// </param>
         public ValueTask<ChampionFullData> GetChampionsAsync(GameVersion version)
         {
@@ -157,14 +158,14 @@ namespace RiotPls.DataDragon
         }
         
         /// <summary>
-        ///     Returns a <see cref="ChampionFullData"/> containing full information
-        ///     about every champion on the game.
+        ///    Returns a <see cref="ChampionFullData"/> containing full information
+        ///    about every champion on the game.
         /// </summary>
         /// <param name="version">
-        ///     The version of Data Dragon to use.
+        ///    The version of Data Dragon to use.
         /// </param>
         /// <param name="language">
-        ///     The language in which the data must be returned. Defaults to English (United States).
+        ///    The language in which the data must be returned. Defaults to English (United States).
         /// </param>
         public ValueTask<ChampionFullData> GetChampionsAsync(GameVersion version, GameLanguage language)
         {
@@ -178,9 +179,10 @@ namespace RiotPls.DataDragon
                     async state =>
                     {
                         var (client, version, language) = state;
-                        var data = await client.MakeRequestAsync<ChampionFullDataDto, ChampionFullData>(
+                        var data = await client.MakeRequestAsync<DataDragonClient, ChampionFullDataDto, ChampionFullData>(
                             $"{Cdn}/{version}/data/{language}/championFull.json", 
-                            dto => new ChampionFullData(client, dto)).ConfigureAwait(false);
+                            client,
+                            (client, dto) => new ChampionFullData(client, dto)).ConfigureAwait(false);
 
                         client._options.FullChampions.Data = data;
                         return data;
@@ -189,14 +191,14 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a <see cref="ChampionData"/> containing full information
-        ///     about a specific champion on the game.
+        ///    Returns a <see cref="ChampionData"/> containing full information
+        ///    about a specific champion on the game.
         /// </summary>
         /// <param name="championName">
-        ///     Name of the champion to query.
+        ///    Name of the champion to query.
         /// </param>
         /// <param name="version">
-        ///    The version of Data Dragon to use.
+        ///   The version of Data Dragon to use.
         /// </param>
         public ValueTask<ChampionData> GetChampionAsync(string championName, GameVersion version)
         {
@@ -208,11 +210,11 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a <see cref="ChampionData"/> containing full information
-        ///     about a specific champion on the game.
+        ///    Returns a <see cref="ChampionData"/> containing full information
+        ///    about a specific champion on the game.
         /// </summary>
         /// <param name="championName">
-        ///     Name of the champion to query.
+        ///    Name of the champion to query.
         /// </param>
         /// <param name="version">
         ///    The version of Data Dragon to use.
@@ -232,9 +234,10 @@ namespace RiotPls.DataDragon
                     async state =>
                     {
                         var (client, championName, version, language) = state;
-                        var data = await client.MakeRequestAsync<ChampionDataDto, ChampionData>(
+                        var data = await client.MakeRequestAsync<DataDragonClient, ChampionDataDto, ChampionData>(
                             $"{Cdn}/{version}/data/{language}/champion/{championName}.json", 
-                            dto => new ChampionData(client, dto)).ConfigureAwait(false);
+                            client,
+                            (client, dto) => new ChampionData(client, dto)).ConfigureAwait(false);
                         
                         client._options._champions.AddOrUpdate(championName,
                             (_, c) => CacheControl<ChampionData>.TimedCache(c._options.ChampionFullCacheDuration),
@@ -247,8 +250,8 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a <see cref="SummonerSpellData"/> containing full information
-        ///     about the whole game's summoner spells
+        ///    Returns a <see cref="SummonerSpellData"/> containing full information
+        ///    about the whole game's summoner spells
         /// </summary>
         /// <param name="version">
         ///    The version of Data Dragon to use.
@@ -263,8 +266,8 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a <see cref="SummonerSpellData"/> containing full information
-        ///     about the whole game's summoner spells
+        ///    Returns a <see cref="SummonerSpellData"/> containing full information
+        ///    about the whole game's summoner spells
         /// </summary>
         /// <param name="version">
         ///    The version of Data Dragon to use.
@@ -284,9 +287,10 @@ namespace RiotPls.DataDragon
                     async state =>
                     {
                         var (client, version, language) = state;
-                        var data = await client.MakeRequestAsync<SummonerSpellDataDto, SummonerSpellData>(
+                        var data = await client.MakeRequestAsync<DataDragonClient, SummonerSpellDataDto, SummonerSpellData>(
                             $"{Cdn}/{version}/data/{language}/summoner.json", 
-                            dto => new SummonerSpellData(client, dto)).ConfigureAwait(false);
+                            client,
+                            (client, dto) => new SummonerSpellData(client, dto)).ConfigureAwait(false);
 
                         client._options.SummonerSpells.Data = data;
                         return data;
@@ -295,11 +299,11 @@ namespace RiotPls.DataDragon
         }
         
         /// <summary>
-        ///     Returns a <see cref="ProfileIconData"/> containing full information
-        ///     about the whole profile icons.
+        ///    Returns a <see cref="ProfileIconData"/> containing full information
+        ///    about the whole profile icons.
         /// </summary>
         /// <param name="version">
-        ///     The version of Data Dragon to use.
+        ///    The version of Data Dragon to use.
         /// </param>
         public ValueTask<ProfileIconData> GetProfileIconsAsync(GameVersion version)
         {
@@ -311,8 +315,8 @@ namespace RiotPls.DataDragon
         }
 
         /// <summary>
-        ///     Returns a <see cref="SummonerSpellData"/> containing full information
-        ///     about the whole game's summoner spells
+        ///    Returns a <see cref="SummonerSpellData"/> containing full information
+        ///    about the whole game's summoner spells
         /// </summary>
         /// <param name="version">
         ///    The version of Data Dragon to use.
@@ -332,30 +336,15 @@ namespace RiotPls.DataDragon
                     async state =>
                     {
                         var (client, version, language) = state;
-                        var data = await client.MakeRequestAsync<ProfileIconDataDto, ProfileIconData>(
+                        var data = await client.MakeRequestAsync<DataDragonClient, ProfileIconDataDto, ProfileIconData>(
                             $"{Cdn}/{version}/data/{language}/profileicon.json", 
-                            dto => new ProfileIconData(client, dto)).ConfigureAwait(false);
+                            client,
+                            (client, dto) => new ProfileIconData(client, dto)).ConfigureAwait(false);
 
                         client._options.ProfileIcons.Data = data;
                         return data;
                     });
             }
-        }
-
-        private async Task<TEntity> MakeRequestAsync<TDto, TEntity>(string url, Func<TDto, TEntity> func)
-            => func(await MakeRequestAsync<TDto>(url).ConfigureAwait(false));
-
-        private async Task<TEntity> MakeRequestAsync<TEntity>(string url)
-        {
-            var request = await _client.GetStreamAsync(url).ConfigureAwait(false);
-            return await JsonSerializer.DeserializeAsync<TEntity>(
-                request, _jsonSerializerOptions).ConfigureAwait(false);
-        }
-
-        private void ThrowIfDisposed()
-        {
-            if (_isDisposed)
-                throw new ObjectDisposedException(GetType().Name);
         }
 
         public void Dispose()
@@ -369,6 +358,22 @@ namespace RiotPls.DataDragon
                 _client.Dispose();
                 _isDisposed = true;
             }
+        }
+
+        private async Task<TEntity> MakeRequestAsync<TState, TDto, TEntity>(string url, TState state, Func<TState, TDto, TEntity> func)
+            => func(state, await MakeRequestAsync<TDto>(url).ConfigureAwait(false));
+
+        private async Task<TEntity> MakeRequestAsync<TEntity>(string url)
+        {
+            var stream = await _client.GetStreamAsync(url).ConfigureAwait(false);
+            return await JsonSerializer.DeserializeAsync<TEntity>(
+                stream, _jsonSerializerOptions).ConfigureAwait(false);
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(GetType().Name);
         }
     }
 }
--- src/RiotPls.DataDragon/Extensions/ChampionExtensions.cs
+++ src/RiotPls.DataDragon/Extensions/ChampionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using RiotPls.DataDragon.Entities;
 
 namespace RiotPls.DataDragon.Extensions
@@ -6,39 +7,48 @@ namespace RiotPls.DataDragon.Extensions
     public static class ChampionExtensions
     {
         /// <summary>
-        ///     Gets a champion base information by its id.
+        ///    Gets a champion base information by its id.
         /// </summary>
         /// <param name="client">
-        ///     Data Dragon client that performs the request.
+        ///    Data Dragon client that performs the request.
         /// </param>
         /// <param name="id">
-        ///     Id of the champion.
+        ///    Id of the champion.
         /// </param>
         /// <param name="version">
-        ///     Version of Data Dragon.
+        ///    Version of Data Dragon.
         /// </param>
-        public static ValueTask<ChampionBase?> GetChampionByIdAsync(this DataDragonClient client,
+        /// <exception cref="ArgumentNullException">
+        ///    Thrown when <paramref name="client"/> is <see langword="null"/>.
+        /// </exception>
+        public static ValueTask<ChampionBase?> GetChampionAsync(this IDataDragonClient client,
             int id, GameVersion version)
-            => GetChampionByIdAsync(client, id, version, client.DefaultLanguage);
-        
+            => client.GetChampionAsync(id, version, client.DefaultLanguage);
+
         /// <summary>
-        ///     Gets a champion base information by its id.
+        ///    Gets a champion base information by its id.
         /// </summary>
         /// <param name="client">
-        ///     Data Dragon client that performs the request.
+        ///    Data Dragon client that performs the request.
         /// </param>
         /// <param name="id">
-        ///     Id of the champion.
+        ///    Id of the champion.
         /// </param>
         /// <param name="version">
-        ///     Version of Data Dragon.
+        ///    Version of Data Dragon.
         /// </param>
         /// <param name="language">
-        ///     Language in which the data must be returned.
+        ///    Language in which the data must be returned.
         /// </param>
-        public static async ValueTask<ChampionBase?> GetChampionByIdAsync(this DataDragonClient client, 
+        /// <exception cref="ArgumentNullException">
+        ///    Thrown when <paramref name="client"/> is <see langword="null"/>.
+        /// </exception>
+        public static async ValueTask<ChampionBase?> GetChampionAsync(this IDataDragonClient client,
             int id, GameVersion version, GameLanguage language)
         {
+            if (client is null)
+                throw new ArgumentNullException(nameof(client));
+
             var champions = await client.GetPartialChampionsAsync(version, language).ConfigureAwait(false);
 
             champions.Champions.TryGetValue(id, out var champion);
