304eb183fa4d76ad65fbed07347b44102e90dda6
Allan <allan.mercou@icloud.com>
2020-07-23 20:14:24 +0200

    Fixes cache for versions and bind items together (#8)
    * Get the item from their ids in From and Into, in Item entity.
    
    * Fixes some issues.

--- src/RiotPls.DataDragon/DataDragonClient.cs
+++ src/RiotPls.DataDragon/DataDragonClient.cs
@@ -63,7 +63,7 @@ namespace RiotPls.DataDragon
 
             lock (_lock)
             {
-                if (_options.CacheMode != CacheMode.None && !_languages.IsDefaultOrEmpty)
+                if (_options.CacheMode != CacheMode.None && !_versions.IsDefaultOrEmpty)
                     return new ValueTask<IReadOnlyList<GameVersion>?>(_versions);
 
                 return new ValueTask<IReadOnlyList<GameVersion>?>(FetchVersionsAsync(token));
@@ -451,6 +451,13 @@ namespace RiotPls.DataDragon
         {
             var data = await FetchBaseDataAsync<ItemDataDto, ItemData>(version, language)
                 .ConfigureAwait(false);
+
+            foreach (var item in data.Items.Values)
+            {
+                item.From = item.FromIds.Select(x => data.Items[x]).ToImmutableArray();
+                item.Into = item.IntoIds.Select(x => data.Items[x]).ToImmutableArray();
+            }
+            
             return data?.Items.Values;
         }
 
